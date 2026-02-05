59ff2ead4c90fdb82731ac3b0f43f48b4405adb0
sreerath-srideep <sreerath2002@gmail.com>
2026-02-05 14:07:24 +0530

    Update and rename placeholder to quality_checks_for_gold.sql

---
+++ tests/quality_checks_for_gold.sql
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------------------------------
+To check whether the tables connect with each other.
+-----------------------------------------------------------------------------------------
+
+select * 
+from gold.fact_sales f
+left join gold.dim_customers c
+on c.customer_key = f.customer_key
+left join gold.dim_products p
+on p.product_key = f.product_key
+where p.product_key is null
